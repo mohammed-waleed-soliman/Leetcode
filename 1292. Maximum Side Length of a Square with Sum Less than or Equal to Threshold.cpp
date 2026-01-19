c3f9f0c9e4622883f6db2e3bd4d467893a3e980b
Mohammed-Waleed-Soliman <wedo.hy@gmail.com>
2026-01-19 22:04:58 +0200

    Maximum Side Length of a Square with Sum Less than or Equal to Threshold problem

---
+++ 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int r = mat.size(), c = mat[0].size(), res = 0;
+        vector<vector<int>>prefix(r+1,vector<int>(c+1,0));
+        for(int i = 1; i <= r; i++)
+        {
+            for(int j = 1; j <= c; j++)
+            {
+                prefix[i][j] = mat[i-1][j-1]+prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1];
+            }
+        }
+        for(int i = 1; i <= r; i++)
+        {
+            for(int j = 1; j <= c; j++)
+            {
+                for(int k = 1; k <= min(r-i+1,c-j+1); k++)
+                {
+                    if(prefix[i+k-1][j+k-1]-prefix[i-1][j+k-1]-prefix[i+k-1][j-1]+prefix[i-1][j-1]<= threshold) res = max(res,k);
+                }
+            }
+        }
+        return res;
+    }
+};
